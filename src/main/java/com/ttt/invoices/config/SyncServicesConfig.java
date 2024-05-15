a7d88bf715f42a3c963f9a6481c264d38290e2da
Ion Ionets <ion.ionets@gmail.com>
2024-05-15 17:39:59 +0100

    jdocs

--- src/main/java/com/ttt/invoices/config/SyncServicesConfig.java
+++ src/main/java/com/ttt/invoices/config/SyncServicesConfig.java
@@ -13,10 +13,18 @@ import java.util.stream.Collectors;
 @Configuration
 public class SyncServicesConfig {
 
+    /**
+     * Creates a map of synchronization strategies, keyed by the class type of the entities
+     * they manage.
+     *
+     * @param syncServices a list of synchronization strategy services that implement the SyncStrategy interface.
+     * @return an unmodifiable map where each key is a class type of the entity and each value
+     * is the corresponding synchronization strategy.
+     */
     @Bean
     public Map<Class<?>, SyncStrategy<?>> syncStrategiesMap(List<SyncStrategy<?>> syncServices) {
         return syncServices.stream()
-                .collect(Collectors.toMap(
+                .collect(Collectors.toUnmodifiableMap(
                         service -> GenericTypeResolver.resolveTypeArgument(service.getClass(), SyncStrategy.class),
                         Function.identity()
                 ));
--- src/main/java/com/ttt/invoices/service/sync/SyncStrategyContext.java
+++ src/main/java/com/ttt/invoices/service/sync/SyncStrategyContext.java
@@ -9,15 +9,42 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Context class that handles synchronization strategies for various entity types.
+ * This class provides methods to save and retrieve entities using the appropriate synchronization
+ * strategy based on the entity type.
+ */
 @Service
 @RequiredArgsConstructor
 public class SyncStrategyContext {
+    /**
+     * Map that holds synchronization strategies keyed by the entity class type.
+     */
     private final Map<Class<?>, SyncStrategy<?>> syncStrategiesMap;
 
+    /**
+     * Saves a list of updated entities using the appropriate synchronization strategy.
+     *
+     * @param clazz the class type of the entities.
+     * @param updated a list of updated entities to be saved.
+     * @param accountingEntityId the ID of the accounting entity associated with the entities.
+     * @param <T> the type of entities extending BaseEntity.
+     */
     public <T extends BaseEntity>  void saveEntities(Class<T> clazz, List updated, long accountingEntityId){
         syncStrategiesMap.get(clazz).saveEntities(updated, accountingEntityId);
     }
 
+
+    /**
+     * Retrieves a batch of updated entities using the appropriate synchronization strategy.
+     *
+     * @param clazz the class type of the entities.
+     * @param accountingEntityId the ID of the accounting entity associated with the entities.
+     * @param lastUpdated the timestamp indicating the last time synchronization was performed.
+     * @param batchSize the maximum number of entities to retrieve in a single batch.
+     * @param <T> the type of entities extending BaseEntity.
+     * @return a SyncResponseDTO containing the updated entities.
+     */
     public <T extends BaseEntity>  SyncResponseDTO<T> getEntities(Class<T> clazz, long accountingEntityId, Instant lastUpdated, int batchSize){
         return (SyncResponseDTO<T>) syncStrategiesMap.get(clazz).getEntities(accountingEntityId, lastUpdated, batchSize);
     }
